14269c2fffc58078a22d2fd9345af94ec5341b40
Mansur Sulejmanov <mansur.sulejmanov@student.ap.be>
2025-04-15 15:13:45 +0200

    herbal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BodyworkComponent } from './bodywork/bodywork.component';
-import { PoetryComponent } from './poetry/poetry.component';
 import { HerbalComponent } from './herbal/herbal.component';
 import { TattooComponent } from './tattoo/tattoo.component';
 import { CommunityComponent } from './community/community.component';
@@ -11,7 +10,6 @@ export const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "about", component: AboutComponent  },
     { path: "bodywork", component: BodyworkComponent},
-    { path: "poetry", component: PoetryComponent},
     { path: "herbal", component: HerbalComponent},
     { path: "tattoo", component: TattooComponent},
     { path: "community", component: CommunityComponent},
