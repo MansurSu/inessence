fcdcf09da8c5843c6ca3b32c10870d1cef994875
Mansur Sulejmanov <mansur.sulejmanov@student.ap.be>
2025-05-06 16:04:47 +0200

    docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { routeTransition } from '../route-transition';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ RouterModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [routeTransition]
 })
 export class AppComponent {
   title = 'inessence';
+  constructor(protected route: ActivatedRoute){}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BodyworkComponent } from './bodywork/bodywork.component';
 import { HerbalComponent } from './herbal/herbal.component';
-import { TattooComponent } from './tattoo/tattoo.component';
 import { CommunityComponent } from './community/community.component';
 
 export const routes: Routes = [
@@ -11,7 +10,6 @@ export const routes: Routes = [
     { path: "about", component: AboutComponent  },
     { path: "bodywork", component: BodyworkComponent},
     { path: "herbal", component: HerbalComponent},
-    { path: "tattoo", component: TattooComponent},
     { path: "community", component: CommunityComponent},
     { path: "", redirectTo: "home", pathMatch:"full"  }, 
     { path: "**", redirectTo:"home", pathMatch:"full" }, 
