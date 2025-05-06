d7b0d02f0af569a34be2d364fd6a5dbadf8db432
Mansur Sulejmanov <mansur.sulejmanov@student.ap.be>
2025-05-06 16:33:23 +0200

    image change + animation delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
-import { routeTransition } from '../route-transition';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ RouterModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  animations: [routeTransition]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'inessence';
